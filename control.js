9a028e32417a26f23cb19d5c7f38a7e3b78b5f33
kiloreux <kiloreux@gmail.com>
2016-02-14 20:32:22 +0100

    Added voice control with API AI

--- control.js
+++ control.js
@@ -1,15 +1,18 @@
-HOST='localhost'
+HOST='10.3.65.166';
+API_KEY='';
+
+
+var config = {
+    server: 'wss://api.api.ai:4435/api/ws/query',
+    token: API_KEY,
+    sessionId: 9291787, //Just a random number
+    onInit: function () {
+        console.log("> ON INIT use config");
+    }
+};
 
 var ros = new ROSLIB.Ros({
     url : 'ws://'+HOST+':9090'
-});
-
-ros.on('connection', function() {
-    console.log('Connected to websocket server.');
-});
-
-ros.on('close', function() {
-    console.log('Connection to websocket server closed.');
 });
 
 var cmdVel = new ROSLIB.Topic({
@@ -18,7 +21,36 @@ var cmdVel = new ROSLIB.Topic({
     messageType : 'geometry_msgs/Twist'
 });
 
-var move = function(linear,angular) {
+var apiAi = new ApiAi(config);
+
+
+var command_it = function() {
+    apiAi.init();
+};
+
+apiAi.onInit = function() {
+    apiAi.open();
+};
+
+apiAi.onOpen = function () {
+    apiAi.startListening();
+    setTimeout(StopListen,4000);//Wait 4 seconds before stopping listening
+};
+
+var StopListen = function() {
+    apiAi.stopListening();
+};
+
+apiAi.onResults = function (data) {
+    voice_command(parseInt(data.result.parameters.steps), 0);
+};
+
+function voice_command (linear, angular) {
+    move(linear,angular);
+    setTimeout(stop,1000);// Wait 1 second before stopping
+};
+
+function move(linear,angular) {
     var twist = new ROSLIB.Message({
         linear: {
             x : linear,
@@ -34,9 +66,7 @@ var move = function(linear,angular) {
     cmdVel.publish(twist);
 };
 
-
-
-var stop = function() {
+function stop() {
     var twist = new ROSLIB.Message({
         linear: {
             x : 0,
